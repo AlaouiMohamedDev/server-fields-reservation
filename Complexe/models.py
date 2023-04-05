82dfccb1ed784d80fc93379ea49af730074b8f3b
HP <elotmoad@gmail.com>
2023-04-05 21:52:28 +0000

    another commit

--- Complexe/models.py
+++ Complexe/models.py
@@ -1,27 +1,33 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
 class Ville(models.Model):
     name = models.CharField(max_length=100)
 class Zone(models.Model):
     name = models.CharField(max_length=100)
     ville = models.ForeignKey(Ville, on_delete=models.CASCADE)
-
 class ComplexeSportif(models.Model):
     name= models.CharField(max_length=100)
     adresse = models.CharField(max_length=250)
     lattitude = models.DecimalField(max_digits = 22,decimal_places = 20)
     longtitude = models.DecimalField(max_digits = 22,decimal_places = 20)
+    description = models.CharField(max_length=250,default=None)
     zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,default=None)
+    url = models.CharField(max_length=250, null=True)
     
+
+class CategoryTerrain(models.Model):
+    typeTerrain = models.CharField(max_length=250)
+    price = models.FloatField()
+    area = models.IntegerField(blank=True, null=True)
+    complexeSportif = models.ForeignKey(ComplexeSportif, on_delete=models.CASCADE,default=None)    
 class Terrain(models.Model):
-    name = models.CharField(max_length=100)
-    rank = models.IntegerField()
-    typeT = models.CharField(max_length=250)
-    description = models.CharField(max_length=250)
-    tarifT = models.CharField(max_length=250)
-    complexeSportif = models.ForeignKey(ComplexeSportif, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100,default=None)
+    rank = models.IntegerField(null=True, blank=True, default=None)
+    number_of_players = models.IntegerField(blank=True, null=True)
+    is_reserved = models.BooleanField(default=False)
+    category = models.ForeignKey(CategoryTerrain, on_delete=models.CASCADE,default=None)
 class Photo(models.Model):
     url = models.CharField(max_length=100)
     terrain = models.ForeignKey(Terrain, on_delete=models.CASCADE)
-
--- Complexe/urls.py
+++ Complexe/urls.py
@@ -1,10 +1,28 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
-
 urlpatterns = [
-    path('Create/',views.complexCreate),
-    path('Delete/<str:pk>/', views.complexDelete),
-    path('Update/<str:pk>/',views.complexUpdate),
-    path('Detail/<str:pk>/',views.complexDetails)
-]
+    path('', views.apiOverview, name = 'api-overview'),
+    path('complexe-list/', views.complexeList, name='complexe-list'),
+    path('complexe-Id/<str:pk>/', views.complexeId, name='complexe-Id'),
+    path('complexe-create/', views.complexeCreate, name='complexe-create'),
+    path('complexe-update/<str:pk>/', views.complexeUpdate, name='complexe-update'),
+    path('complexe-delete/<str:pk>/', views.complexeDelete, name='complexe-delete'),
+    path('field-list/', views.fieldList, name='field-list'),
+    path('field-Id/<str:pk>/', views.fieldId, name='field-Id'),
+    path('field-create/', views.fieldCreate, name='field-create'),
+    path('field-update/<str:pk>/', views.fieldUpdate, name='field-update'),
+    path('field-delete/<str:pk>/', views.fieldDelete, name='field-delete'),
+    path('fieldCategory-list/', views.fieldCategoryList, name='fieldCategory-list'),
+    path('fieldCategory-Id/<str:pk>/', views.fieldCategoryId, name='fieldCategory-Id'),
+    path('fieldCategory-create/', views.fieldCategoryCreate, name='fieldCategory-create'),
+    path('fieldCategory-update/<str:pk>/', views.fieldCategoryUpdate, name='fieldCategory-update'),
+    path('fieldCategory-delete/<str:pk>/', views.fieldCategoryDelete, name='fieldCategory-delete'),
+    path('photo-list/', views.photoList, name='photo-list'),
+    path('photo-Id/<str:pk>/', views.photoId, name='photo-Id'),
+    path('photo-create/', views.photoCreate, name='photo-create'),
+    path('photo-update/<str:pk>/', views.photoUpdate, name='photo-update'),
+    path('photo-delete/<str:pk>/', views.photoDelete, name='photo-delete'),
+    path('list_fields/', views.list_fields, name='list_fields'),
+    ]
